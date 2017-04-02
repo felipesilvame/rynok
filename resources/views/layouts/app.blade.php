b50c1a2590f236e878894ede2f11ca7a12d519f4
Felipe Silva <felipe.silvame@alumnos.usm.cl>
2017-04-01 23:29:45 -0300

    added meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="thumbnail" content="{!! url('/images/portada_rynok.jpg') !!}" />
+    <meta name="author" content="Felipe Silva"/>
+    <meta name="description" content="Rynok es la Bolsa de Valores que administra los activos financieros de las empresas participantes de la XIX Feria de Creación de Empresas. La bolsa fue desarrollada por Wolfies S.A con el fin de máximizar la rentabilidad de las empresas que participan en el mercado bursátil. La rentabilidad de las empresas es nuestra rentabilidad."/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
